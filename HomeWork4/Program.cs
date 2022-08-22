8e41e348a49ee6de8a5cbb431c95ff4ab31c07e1
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 01:33:09 +0300

    HomeWork4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numberOfPictures;
+        int numberOfRows;
+        int numberOfFullRows;
+        int leftPictures;
+
+        Console.Write("Введите количество картинок: ");
+        numberOfPictures = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество столбцов: ");
+        numberOfRows = Convert.ToInt32(Console.ReadLine());
+
+        numberOfFullRows = numberOfPictures / numberOfRows;
+        leftPictures = numberOfPictures - numberOfFullRows * numberOfRows;
+
+        Console.WriteLine($"Можно будет вывести {numberOfFullRows} рядов и сверх меры будет {leftPictures} картинок");
+    }
+}
